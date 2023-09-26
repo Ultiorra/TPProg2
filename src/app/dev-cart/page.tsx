ba3532fa7c245e7b68676261f4ccf28536a55368
Ultiorra <91594501+Ultiorra@users.noreply.github.com>
2023-09-26 14:37:44 +0200

    1.2
    uwu

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
+import { ProductCartLine } from "../../../tp-kit/components/products/product-cart-line";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 export default function DevCartPage() {
@@ -23,7 +24,31 @@ export default function DevCartPage() {
 
       {/* Panier */}
       <section className="w-full lg:w-1/3 space-y-8">
-				
+				<ProductCartLine
+          product={products[0]}
+          qty={1}
+          onQtyChange={(quantity) => console.log(quantity)}
+          onDelete={() => console.log("delete")}/>
+        <ProductCartLine
+          product={products[1]}
+          qty={2}
+          onQtyChange={(quantity) => console.log(quantity)}
+          onDelete={() => console.log("delete")}/>
+         <div className={` text-sm`}>
+          <div className="flex flex-row gap-4 justify-between items-center mb-2">
+            <h3 className="w-full">Total</h3>
+            <span className="text-right"> {
+              products[0].price * 1 + products[1].price * 2
+            }
+            </span>
+          </div>
+          </div>
+
+          <Button variant={"primary"} fullWidth>Commander</Button>
+
+        
+          
+
 				<Button variant={"outline"} fullWidth>Vider le panier</Button>
 			</section>
       {/* /Panier */}
