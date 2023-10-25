dd85f7599177781003821cbed51e9174c949dbc4
Ultiorra <91594501+Ultiorra@users.noreply.github.com>
2023-10-25 15:13:16 +0200

    3.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 import { BreadCrumbs, SectionContainer } from "tp-kit/components";
-import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../components/product-list";
 import { Metadata } from "next";
-const categories = PRODUCTS_CATEGORY_DATA;
+import prisma from "../../prisma/prisma";
+import { ProductsCategoryData } from "tp-kit/types";
+
 
 export const metadata:Metadata = {
   title: `Page d’accueil - Starbucks`,
   description: "Commandez de délicieuses boissons préparées avec soin par nos baristas"
 }
 
-export default function Home() {
+export default async function Home() {
+  const categories =  await prisma.productCategory.findMany({
+    include: {
+      products: true,
+    },
+  }) 
+  
   return (<SectionContainer>
     <BreadCrumbs items={[
       {
