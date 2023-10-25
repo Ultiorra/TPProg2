1e4f3ec3d8b2b2bcc4ef3dd1401a110a37bb6227
Ultiorra <91594501+Ultiorra@users.noreply.github.com>
2023-10-25 15:26:38 +0200

    2.3

--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -3,20 +3,45 @@ import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../../components/product-list";
 import { NextPageProps } from "../../types";
 import { Metadata } from "next";
-const category = PRODUCTS_CATEGORY_DATA[0];
+import prisma from "../../../prisma/prisma";
+import { ProductsCategoryData } from "tp-kit/types";
+import { notFound } from "next/navigation";
+
 
 type Props = {
   categorySlug: string;
 };
+async function getCategory(categorySlug: string) {
+  const categoryCache: { [categorySlug: string]: ProductsCategoryData | null } = {};
+  if (categoryCache[categorySlug] !== undefined) {
+    return categoryCache[categorySlug]; 
+  }
+  const category = await prisma.productCategory.findFirst({
+    where: {
+      slug: categorySlug
+    },
+    include: {
+      products: true,
+    },
+  })  
+  categoryCache[categorySlug] = category
+  if (!category) {
+    return notFound()
+  }
+
+  return category
+}
 
 export async function generateMetadata({ params, searchParams} : NextPageProps<Props>) : Promise<Metadata> {
+  const category = await getCategory(params.categorySlug)
   return {
-    title: category.name,
+    title: category?.name || "Page d’accueil - Starbucks",
     description: `Trouvez votre inspiration avec un vaste choix de boissons Starbucks parmi nos produits ${category.name}`
   }
 }
 
-export default function CategoryPage({params}: NextPageProps<Props>) {
+export default async function CategoryPage({params}: NextPageProps<Props>) {
+  const category = await getCategory(params.categorySlug)
   return <SectionContainer>
     <BreadCrumbs 
       items={[
